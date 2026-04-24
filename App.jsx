a4d15608349e971249eff3b7575bc4f6b1de4782
martinkelly782-design <martinkelly782@gmail.com>
2026-04-25 01:01:59 +0700

    Delete App.jsx

--- App.jsx
+++
@@ -1,8 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 128 128">
-  <rect width="128" height="128" rx="24" fill="#0b2234"/>
-  <path d="M64 18v76" stroke="#d6b25e" stroke-width="7" stroke-linecap="round"/>
-  <path d="M38 32c8 8 13 18 13 31 0 15-5 27-13 36" fill="none" stroke="#d6b25e" stroke-width="7" stroke-linecap="round"/>
-  <path d="M90 32c-8 8-13 18-13 31 0 15 5 27 13 36" fill="none" stroke="#d6b25e" stroke-width="7" stroke-linecap="round"/>
-  <path d="M30 56h68" stroke="#d6b25e" stroke-width="7" stroke-linecap="round"/>
-  <path d="M52 108h24" stroke="#d6b25e" stroke-width="7" stroke-linecap="round"/>
-</svg>
